d64f68c7808d8483afa3e61f197119ab9b2114fb
Sudha Senthilkumar <ssenthilkumar1@studentmail.nait.ca>
2015-09-23 20:42:08 -0600

    sampleTest created

---
+++ SampleTestWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleTestWebsite.Startup))]
+namespace SampleTestWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
